396575ec3a299bcd18e3396e0706ffd292c8ee67
davidddeveloper <programmingdonebydavid@gmail.com>
2024-05-22 01:53:19 -1200

    staticfiledirs

--- Personal/settings.py
+++ Personal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#l^gyhw-b8lnv&9v$&&*byk#&5k^0b1fw*m17o7&pol$3-1=ll
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['davidconteh.pythonanywhere.com']
+ALLOWED_HOSTS = ['davidconteh.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
